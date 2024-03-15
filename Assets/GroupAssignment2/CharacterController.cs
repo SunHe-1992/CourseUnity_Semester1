26c72bbedc47ee54bac13be916e36c71b7bb5955
Sun He <claymore.sun@gmail.com>
2024-03-14 21:28:21 -0400

    group assignment2 functions, (need to replace Images)

---
+++ Assets/GroupAssignment2/CharacterController.cs
@@ -0,0 +1,40 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CharacterController : MonoBehaviour
+{
+
+    Animator animator;
+    // Start is called before the first frame update
+    void Start()
+    {
+        animator = GetComponent<Animator>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Space))
+            animator.SetBool("isJump", true);
+        if (Input.GetKeyUp(KeyCode.Space))
+            animator.SetBool("isJump", false);
+
+        if (Input.GetKeyDown(KeyCode.J))
+            animator.SetBool("isAttack", true);
+        if (Input.GetKeyUp(KeyCode.J))
+            animator.SetBool("isAttack", false);
+        if (Input.GetKey(KeyCode.K))
+            animator.SetBool("isCrouch", true);
+        else
+            animator.SetBool("isCrouch", false);
+
+
+        float hAxis = Mathf.Abs(Input.GetAxis("Horizontal"));
+        if (hAxis > 0.5f)
+            animator.SetFloat("speed", hAxis);
+        else
+            animator.SetFloat("speed", 0);
+
+    }
+}
